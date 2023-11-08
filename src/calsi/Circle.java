b5fee826ebe955ec362a1f6766f351c4a9adc8bc
wasvi <wasvisahil09@gmail.com>
2023-11-08 11:03:17 +0530

    v1

--- src/calsi/Circle.java
+++ src/calsi/Circle.java
@@ -1,5 +1,14 @@
 package calsi;
-
-public class Circle {
-
+import java.util.Scanner;
+public class Circle{
+    public static void main(String[] args)
+ 
+{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle: ");
+        double radius = sc.nextDouble();
+       double pi = Math.PI;
+        double area = pi * radius * radius;
+        System.out.println("The area of the circle is: " + area);
+    }
 }
